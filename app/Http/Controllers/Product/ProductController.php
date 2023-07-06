839ec9955b9dedc28ba000677800fd101ad2cea7
chien-doxuan <chien.doxuan@vti.com.vn>
2023-07-06 16:10:31 +0700

    [feature] add base response

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Presenter\BaseResponse;
 use App\UseCase\Product\ProductUseCase;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
@@ -19,7 +20,14 @@ class ProductController extends Controller
 
     public function index(Request $request)
     {
-        return response()->json(["status" => 200]);
+        //Example
+        $statusCode = Response::HTTP_OK;
+        $message = 'SUCCESS';
+        $result = [
+            'name' => 'Product 1',
+        ];
+
+        return BaseResponse::basicResponse($statusCode, $message, $result);
     }
 
 }
