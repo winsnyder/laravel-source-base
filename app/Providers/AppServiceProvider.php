d143f412976903b2e76ad6c258a6e56ea5488979
V001694 <kien.nguyenduy.vnu@gmail.com>
2023-07-06 16:00:09 +0700

    add feature get log query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.debug')) {
+            DB::listen(function ($query) {
+                $sqlWithPlaceholders = str_replace(['%', '?', '%s%s'], ['%%', '%s', '?'], $query->sql);
+
+                $bindings = $query->connection->prepareBindings($query->bindings);
+                $pdo = $query->connection->getPdo();
+                $realSql = $sqlWithPlaceholders;
+                // $duration = $this->formatDuration($query->time / 1000);
+
+                if (count($bindings) > 0) {
+                    $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+                }
+
+                error_log($realSql);
+            });
+        }
     }
 }
